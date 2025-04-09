0d40cfa5d51fbd430f9c89ec18216e7d4655d50b
Leo <146775205+LeoAboard@users.noreply.github.com>
2025-04-09 08:55:52 -0400

    Add files via upload

--- interface.py
+++ interface.py
@@ -1,25 +1,34 @@
 from tkinter import * #sudo apt-get install python3-tk
 
+
+root = Tk()
+
 class application:
     def __init__(self, master=None):
 
+        root.title("Calculadora de Momento de Inércia")
+        root.attributes('-fullscreen', True)
+        root.configure(background='#1e3743')
+
         #-------------------------------------------------CRIAÇÃO DE CONTAINERS--------------------------------------------------#
 
-        self.widget1 = Frame(master)
-        self.widget1["pady"] = 10
-        self.widget1.pack()
+        self.widget1 = Frame(master)   #frame titulo
+        self.widget1["pady"] = 10    
+        self.widget1.pack(fill=X)
 
-        self.widget2 = Frame(master)
-        self.widget2["padx"] = 20
-        self.widget2.pack()
+        self.widget2 = Frame(master, width=500)   #frame entradas
+        self.widget2["padx"] = 10
+        self.widget2.columnconfigure(0, weight=1)
+        self.widget2.pack_propagate(0)       
+        self.widget2.pack(side=LEFT, fill=Y)
 
-        self.widget3 = Frame(master)
-        self.widget3["padx"] = 20
+        self.widget3 = Frame(master)   #frame visual
+        self.widget3["padx"] = 20   
+        self.widget3["pady"] = 20
         self.widget3.pack()
 
-        self.widget4 = Frame(master)
-        self.widget4["pady"] = 20
-        self.widget4.pack()
+        self.widget4 = Frame(self.widget2)       #frame titulo parte superior
+        self.widget4.pack(side=TOP)
 
         #---------------------------------------------------TEXTOS-----------------------------------------------------------#
 
@@ -27,45 +36,90 @@ class application:
         self.titulo["font"] = ("Arial", "12", "bold")
         self.titulo.pack()
 
-        self.resultado = Label(self.widget4, text="")
+        self.resultado = Label(self.widget2)
         self.resultado["font"] = ("Arial", "10", "bold")
-        self.resultado.pack()
+        self.resultado.pack(side=BOTTOM)
 
         #---------------------------------------------------ENTRADAS-----------------------------------------------------------#
 
-        self.baseLabel = Label(self.widget2, text="Base (m)", font=("Arial", "12"))
-        self.baseLabel.pack(side=LEFT)
+        self.Y = Entry(self.widget4)
+        self.Y["width"] = 10
+        self.Y["font"] = ("Arial", "12")
+        self.Y.grid(row=1, column=3, padx=5, pady=10)
 
-        self.base = Entry(self.widget2)
+        self.CXLabel = Label(self.widget4, text="X Inicial", font=("Arial", "12"))
+        self.CXLabel.grid(row=0, column=0)
+
+        self.CX = Entry(self.widget4)
+        self.CX["width"] = 10
+        self.CX["font"] = ("Arial", "12")
+        self.CX.grid(row=1, column=0, padx=5, pady=10)
+
+        self.CYLabel = Label(self.widget4, text="Y Inicial", font=("Arial", "12"))
+        self.CYLabel.grid(row=0, column=1)
+
+        self.XLabel = Label(self.widget4, text="Centróide X", font=("Arial", "12"))
+        self.XLabel.grid(row=0, column=2)
+
+        self.X = Entry(self.widget4)
+        self.X["width"] = 10
+        self.X["font"] = ("Arial", "12")
+        self.X.grid(row=1, column=2, padx=5, pady=10)
+
+        self.YLabel = Label(self.widget4, text="Centróide Y", font=("Arial", "12"))
+        self.YLabel.grid(row=0, column=3)
+
+        self.CY = Entry(self.widget4)
+        self.CY["width"] = 10
+        self.CY["font"] = ("Arial", "12")
+        self.CY.grid(row=1, column=1, padx=5, pady=10)
+
+        self.baseLabel = Label(self.widget4, text="Base (m)", font=("Arial", "12"))
+        self.baseLabel.grid(row=2, column=0)
+
+        self.base = Entry(self.widget4)
         self.base["width"] = 10
         self.base["font"] = ("Arial", "12")
-        self.base.pack(side=LEFT)           #não estou conseguindo mudar a posição dos textos nem das entradas :(
+        self.base.grid(row=3, column=0, padx=5, pady=10)
 
-        self.alturaLabel = Label(self.widget3, text="Altura (m)", font=("Arial", "12"))
-        self.alturaLabel.pack(side=LEFT)
+        self.alturaLabel = Label(self.widget4, text="Altura (m)", font=("Arial", "12"))
+        self.alturaLabel.grid(row=2, column=1)
 
-        self.altura = Entry(self.widget3)
+        self.altura = Entry(self.widget4)
         self.altura["width"] = 10
         self.altura["font"] = ("Arial", "12")
-        self.altura.pack(side=LEFT)
+        self.altura.grid(row=3, column=1, padx=5, pady=10)
 
         #---------------------------------------------------BOTÕES-------------------------------------------------------------#
 
         self.contar = Button(self.widget4)
         self.contar["text"] = "Calcular"
-        self.contar["font"] = ("Calibri", "10")
+        self.contar["font"] = ("Arial", "12")
         self.contar["fg"] = ("Blue")
         self.contar["width"] = 10
+        self.contar["pady"] = 5
         self.contar["command"] = self.Calcular #atrela botão a função
-        self.contar.pack()
+        self.contar.grid(row=4, column=1, columnspan=2)
 
-        self.sair = Button(self.widget4)
+        self.sair = Button(self.widget2)
         self.sair["text"] = "Sair"
-        self.sair["font"] = ("Calibri", "10")
+        self.sair["font"] = ("Arial", "12")
         self.sair["fg"] = ("Red")
         self.sair["width"] = 5
-        self.sair["command"] = self.widget4.quit  #atribuição de uma função "quit" para o botão
-        self.sair.pack()
+        self.sair["command"] = self.widget2.quit  #atribuição de uma função "quit" para o botão
+        self.sair.pack(side=BOTTOM)
+
+        self.add = Button(self.widget4)
+        self.add["text"] = "Adicionar"
+        self.add["font"] = ("Arial", "12")
+        self.add["width"] = 7
+        self.add.grid(row=3, column=2)
+
+        self.sub = Button(self.widget4)
+        self.sub["text"] = "Subtrair"
+        self.sub["font"] = ("Arial", "12")
+        self.sub["width"] = 7
+        self.sub.grid(row=3, column=3)
 
         #----------------------------------------------------------------------------------------------------------------------#
 
@@ -81,6 +135,5 @@ class application:
 
 
 
-root = Tk()
 application(root)
 root.mainloop()              #necessario para rodar a GUI
